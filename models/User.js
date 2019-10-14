9b9a2ffcd4c81482a57ec0fd60be8232567665f3
epowell7 <ericpowell777@gmail.com>
2019-10-13 22:27:53 -0400

    added music and fixed a couple bugs

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { type: String, required: true },
-    hash: { type: String, required: true },
+    password: { type: String, required: true },
     sav: { type: String, required: true },
 });
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 // Dependencies
 const router = require("express").Router();
 const userRoutes = require("./users");
+const messageRoutes = require("./messages");
 
 // User routes
 router.use("/users", userRoutes);
+router.use("/messages", messageRoutes);
 
 module.exports = router;
