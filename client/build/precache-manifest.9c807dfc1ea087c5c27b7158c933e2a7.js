985b339cde8c808db970ccc9932660776c2ff2d9
epowell7 <ericpowell777@gmail.com>
2019-10-17 19:35:54 -0400

    work please

---
+++ client/build/precache-manifest.9c807dfc1ea087c5c27b7158c933e2a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bc0cc7545bacfe7a6b00836a12b031",
+    "url": "/ericpowell3d/Zone-Unknown-React/index.html"
+  },
+  {
+    "revision": "56a0a314e13b47bea1b4",
+    "url": "/ericpowell3d/Zone-Unknown-React/static/js/2.ad1f5818.chunk.js"
+  },
+  {
+    "revision": "745fda7673358c545eac",
+    "url": "/ericpowell3d/Zone-Unknown-React/static/js/main.0ce8e0ab.chunk.js"
+  },
+  {
+    "revision": "c7a327ab24ebce20a5b2",
+    "url": "/ericpowell3d/Zone-Unknown-React/static/js/runtime-main.ffcccae6.js"
+  }
+]);
--- database.js
+++ database.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 
 // Connect to Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/zu_db', { useUnifiedTopology: true, useNewUrlParser: true });
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://user1:password1@ds235378.mlab.com:35378/heroku_8zlt8b4d', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://user1:password1@ds235378.mlab.com:35378/heroku_8zlt8b4d' || 'mongodb://localhost:27017/zu_db', { useUnifiedTopology: true, useNewUrlParser: true });
 
 module.exports = mongoose.connection;
