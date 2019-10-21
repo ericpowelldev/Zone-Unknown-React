86a747c6280251271ce971913858f8dcb2fbb108
epowell7 <ericpowell777@gmail.com>
2019-10-21 19:51:02 -0400

    fixed a redirect bug

--- models/Message.js
+++ models/Message.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-mongoose.promise = Promise;
 
 const messageSchema = new Schema({
     author: { type: String, required: true },
